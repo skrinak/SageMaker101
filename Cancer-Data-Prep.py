2b3928797c958fc27e0fc72a92d0cf3545cc67f8
Kris Skrinak <skrinak@amazon.com>
2018-01-18 09:32:28 +0000

    Tweaks from 2nd London session

--- Cancer-Data-Prep.py
+++ Cancer-Data-Prep.py
@@ -1,13 +1,19 @@
-import boto3
-import numpy as np
-import pandas as pd
-import io
 import os
-import sagemaker.amazon.common as smac
+import boto3
+import re
 from sagemaker import get_execution_role
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import io
+import time
+import json
+import sagemaker.amazon.common as smac
+
+bucket = 'tenacious'# enter your s3 bucket where you will copy data and model artifacts
+prefix = 'sagemaker/breast_cancer_prediction' # place to upload training files within the bucket
 
 role = get_execution_role()
-print(role)
 
 data = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data', header = None)
 
@@ -19,7 +25,6 @@ data.columns = ["id","diagnosis","radius_mean","texture_mean","perimeter_mean","
                 "perimeter_worst","area_worst","smoothness_worst","compactness_worst","concavity_worst",
                 "concave points_worst","symmetry_worst","fractal_dimension_worst"] 
 
-
 # save the data
 data.to_csv("data.csv", sep=',', index=False)
 
@@ -27,7 +32,16 @@ data.to_csv("data.csv", sep=',', index=False)
 print(data.shape)
 
 # show the top few rows
-display(data.head())
+# display(data.head())
+print(data.head())
+
+# describe the data object
+# display(data.describe())
+print(data.describe())
+
+# we will also summarize the categorical field diganosis 
+# display(data.diagnosis.value_counts())
+print(data.diagnosis.value_counts())
 
 rand_split = np.random.rand(len(data))
 train_list = rand_split < 0.8
@@ -47,9 +61,7 @@ val_X = data_val.iloc[:,2:].as_matrix();
 test_y = ((data_test.iloc[:,1] == 'M') +0).as_matrix();
 test_X = data_test.iloc[:,2:].as_matrix();
 
-bucket = '2017-sagemaker'# enter your s3 bucket where you will copy data and model artifacts
-prefix = 'sagemaker/breast_cancer_prediction' # place to upload training files within the bucket
-
+###########################################################
 train_file = 'linear_train.data'
 
 f = io.BytesIO()
@@ -58,6 +70,7 @@ f.seek(0)
 
 boto3.Session().resource('s3').Bucket(bucket).Object(os.path.join(prefix, 'train', train_file)).upload_fileobj(f)
 
+###########################################################
 validation_file = 'linear_validation.data'
 
 f = io.BytesIO()
@@ -66,8 +79,10 @@ f.seek(0)
 
 boto3.Session().resource('s3').Bucket(bucket).Object(os.path.join(prefix, 'validation', train_file)).upload_fileobj(f)
 
+############################################
 test_file = 'test_data.csv'
 
 data_test.to_csv(test_file, index=False)
 with open(test_file, 'rb') as data:
     boto3.Session().resource('s3').Bucket(bucket).upload_fileobj(data, os.path.join(prefix, 'test', test_file))
+
